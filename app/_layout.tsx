2265ec2b4f606ea71adb5f7211606d3cabc2d41a
Pierre MOULIN <pierre.moulin@inter-actif.net>
2023-11-13 13:34:56 +0100

    feat(ui): locks orientation to landscape

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,30 @@
+import { useEffect } from "react"
+
 import { Slot } from "expo-router"
 
 import { useFonts } from "expo-font"
+import * as ScreenOrientation from "expo-screen-orientation"
 
 import fonts from "assets/fonts"
 import LoadingScreen from "screens/LoadingScreen"
 
 import AuthContainer from "../providers/AuthProvider"
 
+let didInit = false
+
 export default function RootLayout() {
   const [fontsLoaded] = useFonts(fonts)
+
+  const lockOrientation = async () => {
+    await ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE_LEFT)
+  }
+
+  useEffect(() => {
+    if (!didInit) {
+      didInit = true
+      lockOrientation()
+    }
+  }, [])
 
   if (!fontsLoaded) {
     return <LoadingScreen />
