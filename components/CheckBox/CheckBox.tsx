28bd70b7f5526e5b6710bab0219c0f25a3bd9ffd
Pierre Moulin <phv.moulin@gmail.com>
2024-05-30 16:00:43 +0200

    fix(ui): :lipstick: use touchable with feedback for checkbox

--- components/CheckBox/CheckBox.tsx
+++ components/CheckBox/CheckBox.tsx
@@ -1,9 +1,9 @@
 import React from "react"
-import { Pressable, PressableProps, StyleProp, View, ViewStyle } from "react-native"
+import { StyleProp, TouchableOpacity, TouchableOpacityProps, View, ViewStyle } from "react-native"
 
 import styles from "./CheckBox.styles"
 
-type CheckBoxProps = PressableProps & {
+type CheckBoxProps = TouchableOpacityProps & {
   containerStyle?: StyleProp<ViewStyle>
   style?: StyleProp<ViewStyle>
   checkedStyle?: StyleProp<ViewStyle>
@@ -14,10 +14,13 @@ type CheckBoxProps = PressableProps & {
 export default function CheckBox(props: CheckBoxProps) {
   const { size = 18, containerStyle, style, checkedStyle, isChecked = false, ...rest } = props
   return (
-    <Pressable style={[styles.container, containerStyle, { width: size, height: size }]} {...rest}>
+    <TouchableOpacity
+      style={[styles.container, containerStyle, { width: size, height: size }]}
+      {...rest}
+    >
       <View
         style={[styles.content, style, isChecked && styles.checked, isChecked && checkedStyle]}
       />
-    </Pressable>
+    </TouchableOpacity>
   )
 }
