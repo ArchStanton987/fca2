247c5b859c717749915e12aa2363c4a481e6f817
Pierre MOULIN <pierre.moulin@inter-actif.net>
2024-05-31 13:50:27 +0200

    📝 doc: update todos

--- config/firebase-env.ts
+++ config/firebase-env.ts
@@ -2,9 +2,6 @@
 import { initializeApp } from "firebase/app"
 import { getDatabase } from "firebase/database"
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 const firebaseConfig = {
   apiKey: process.env.EXPO_PUBLIC_FB_API_KEY,
   authDomain: process.env.EXPO_PUBLIC_FB_AUTH_DOMAIN,
