b6f0c347ccb57206612b80e3f4dfacb94772f028
Pierre MOULIN <pierre.moulin@inter-actif.net>
2024-06-19 14:00:44 +0200

    ✨ feat(combat): add action points management

--- app/squad/[squadId]/character/[charId]/(nav)/combat/_layout.tsx
+++ app/squad/[squadId]/character/[charId]/(nav)/combat/_layout.tsx
@@ -1,14 +1,32 @@
-import { Stack, useLocalSearchParams } from "expo-router"
+import { Tabs, useLocalSearchParams } from "expo-router"
 
+import Header from "components/Header/Header"
+import { HeaderElementId } from "components/Header/Header.utils"
+import TabBar from "components/TabBar/TabBar"
 import colors from "styles/colors"
+
+const datetime: HeaderElementId[] = ["date", "time"]
 
 export default function CombatLayout() {
   const { squadId, charId } = useLocalSearchParams<{ squadId: string; charId: string }>()
   return (
-    <Stack
-      screenOptions={{ headerShown: false, contentStyle: { backgroundColor: colors.primColor } }}
+    <Tabs
+      // eslint-disable-next-line react/no-unstable-nested-components
+      tabBar={props => <TabBar tabBarId="main" {...props} />}
+      screenOptions={{
+        headerShown: false,
+        // eslint-disable-next-line react/no-unstable-nested-components
+        header: props => (
+          <Header headerElementsIds={[...datetime, "progression", "rads", "hp"]} {...props} />
+        )
+      }}
+      sceneContainerStyle={{ backgroundColor: colors.primColor }}
     >
-      <Stack.Screen name="recap" initialParams={{ squadId, charId }} />
-    </Stack>
+      <Tabs.Screen
+        name="combat"
+        options={{ title: "Bagarre" }}
+        initialParams={{ squadId, charId }}
+      />
+    </Tabs>
   )
 }
--- screens/CombatScreen/CombatScreen.tsx
+++ screens/CombatScreen/CombatScreen.tsx
@@ -1,12 +1,47 @@
 import React from "react"
 import { View } from "react-native"
 
+import useCases from "lib/common/use-cases"
+
+import CheckBox from "components/CheckBox/CheckBox"
+import DrawerPage from "components/DrawerPage"
+import Spacer from "components/Spacer"
 import Txt from "components/Txt"
+import { useCharacter } from "contexts/CharacterContext"
 
 export default function CombatScreen() {
+  const character = useCharacter()
+  const { status, secAttr } = character
+  const { currAp } = status
+  const maxAp = secAttr.curr.actionPoints
+
+  const handleSetAp = async (i: number) => {
+    const newValue = i < currAp ? i : i + 1
+    await useCases.status.updateElement(character, "currAp", newValue)
+  }
+
+  const apArr = []
+  for (let i = 0; i < maxAp; i += 1) {
+    apArr.push(`pa${i}`)
+  }
+
   return (
-    <View>
-      <Txt>CombatScreen</Txt>
-    </View>
+    <DrawerPage>
+      <Txt>Points d&apos;action</Txt>
+      <Spacer y={20} />
+
+      <View
+        style={{
+          flex: 1,
+          flexDirection: "row",
+          justifyContent: "space-evenly",
+          alignItems: "center"
+        }}
+      >
+        {apArr.map((ap, i) => (
+          <CheckBox key={ap} isChecked={i < currAp} onPress={() => handleSetAp(i)} />
+        ))}
+      </View>
+    </DrawerPage>
   )
 }
