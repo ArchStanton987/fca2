a2a173ca03d3766cb2d31f2fddef619e9b6ec26d
Pierre MOULIN <pierre.moulin@inter-actif.net>
2024-09-02 13:39:22 +0200

    ✨ feat(engine): add weapons use case import

--- lib/common/use-cases.ts
+++ lib/common/use-cases.ts
@@ -1,6 +1,7 @@
 import getAbilitiesUseCases from "lib/character/abilities/abilities-use-cases"
 import getEffectsUseCases from "lib/character/effects/effects-use-cases"
 import getStatusUseCases from "lib/character/status/status-use-cases"
+import getWeaponsUseCases from "lib/objects/data/weapons/weapons-use-cases"
 import getEquipedObjectsUseCases from "lib/objects/equiped-objects-use-cases"
 import getInventoryUseCases from "lib/objects/inventory-use-cases"
 import getSquadUseCases from "lib/squad/squad-use-cases"
@@ -8,6 +9,7 @@ import getSquadUseCases from "lib/squad/squad-use-cases"
 const effects = getEffectsUseCases()
 const equipedObjects = getEquipedObjectsUseCases()
 const inventory = getInventoryUseCases()
+const weapons = getWeaponsUseCases()
 const status = getStatusUseCases()
 const squad = getSquadUseCases()
 const abilities = getAbilitiesUseCases()
@@ -16,6 +18,7 @@ const useCases = {
   effects,
   equipedObjects,
   inventory,
+  weapons,
   status,
   squad,
   abilities
