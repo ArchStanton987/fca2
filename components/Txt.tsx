8f445dccff5c2560f2083676d88c885a9e047b63
Pierre Moulin <phv.moulin@gmail.com>
2024-09-21 18:01:09 +0200

    perf(nav): :zap: remove animation on texts

--- components/Txt.tsx
+++ components/Txt.tsx
@@ -1,47 +1,9 @@
 import { Text, TextProps } from "react-native"
 
-import Animated, { useAnimatedStyle, useDerivedValue, withTiming } from "react-native-reanimated"
-
 import colors from "styles/colors"
 import typos from "styles/typos"
 
-interface TxtProps extends TextProps {
-  withRevertColorAnim?: boolean
-  isPressed?: boolean
-  initAnimColor?: string
-  endAnimColor?: string
-}
-
-export default function Txt({
-  children,
-  style,
-  withRevertColorAnim = true,
-  isPressed,
-  initAnimColor = colors.secColor,
-  endAnimColor = colors.primColor,
-  ...rest
-}: TxtProps) {
-  const textColor = useDerivedValue(() =>
-    isPressed
-      ? withTiming(endAnimColor, { duration: 200 })
-      : withTiming(initAnimColor, { duration: 200 })
-  )
-
-  const textAnimatedStyle = useAnimatedStyle(() => ({
-    color: textColor.value
-  }))
-
-  if (withRevertColorAnim) {
-    return (
-      <Animated.Text
-        style={[{ fontFamily: typos.monofonto, color: colors.secColor }, textAnimatedStyle, style]}
-        {...rest}
-      >
-        {children}
-      </Animated.Text>
-    )
-  }
-
+export default function Txt({ children, style, ...rest }: TextProps) {
   return (
     <Text style={[{ fontFamily: typos.monofonto, color: colors.secColor }, style]} {...rest}>
       {children}
