69ca09f38a0c7917994eb2324ca317cc7c4e9465
Pierre Moulin <phv.moulin@gmail.com>
2024-10-14 18:55:59 +0200

    refactor: :coffin: remove dead code

--- screens/InventoryTabs/MiscObjScreen/MiscObjDetails.tsx
+++ screens/InventoryTabs/MiscObjScreen/MiscObjDetails.tsx
@@ -1,7 +1,6 @@
 import React from "react"
 import { ScrollView } from "react-native"
 
-import { changeableAttributesMap } from "lib/character/effects/changeable-attr"
 import { MiscObject } from "lib/objects/data/misc-objects/misc-objects-types"
 
 import Spacer from "components/Spacer"
@@ -19,7 +18,7 @@ function Header() {
 export default function MiscObjDetails({ miscObj }: { miscObj: MiscObject | null }) {
   if (!miscObj) return <Header />
 
-  const { description, symptoms } = miscObj.data
+  const { description } = miscObj.data
 
   return (
     <>
@@ -28,18 +27,6 @@ export default function MiscObjDetails({ miscObj }: { miscObj: MiscObject | null
         <ScrollView>
           <Txt>{description}</Txt>
           <Spacer y={10} />
-          {symptoms.length > 0
-            ? symptoms.map(symptom => {
-                const { id, value } = symptom
-                const prepend = value > 0 ? "+" : ""
-                return (
-                  <Txt key={id}>
-                    {changeableAttributesMap[id].short}: {prepend}
-                    {value}
-                  </Txt>
-                )
-              })
-            : null}
         </ScrollView>
       ) : null}
     </>
