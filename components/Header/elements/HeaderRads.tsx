9eeafb1bfc234fd797f17d4f3f0b39d356d38b5a
Pierre Moulin <phv.moulin@gmail.com>
2024-12-12 17:52:17 +0100

    fix(ui): :heavy_minus_sign: uses expo icons for radiation

--- components/Header/elements/HeaderRads.tsx
+++ components/Header/elements/HeaderRads.tsx
@@ -2,12 +2,12 @@ import { TouchableOpacity } from "react-native"
 
 import { router } from "expo-router"
 
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5"
 import { radStates } from "lib/character/health/health"
 
 import HeaderElement from "components/Header/HeaderElement"
 import Spacer from "components/Spacer"
 import Txt from "components/Txt"
-import RadsIcon from "components/icons/RadsIcon"
 import routes from "constants/routes"
 import { useCharacter } from "contexts/CharacterContext"
 import { useSquad } from "contexts/SquadContext"
@@ -33,7 +33,7 @@ export default function HeaderRads() {
   return (
     <TouchableOpacity onPress={onPress}>
       <HeaderElement>
-        <RadsIcon color={getColor()} size={12} />
+        <FontAwesome5 name="radiation" size={12} color={getColor()} />
         <Spacer x={5} />
         <Txt style={{ color: getColor(), fontSize: 12 }}>{health.rads}</Txt>
       </HeaderElement>
