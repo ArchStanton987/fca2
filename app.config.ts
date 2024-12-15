dce99bce523d07434c1cae0fb29f62ed31ee1fb7
Pierre Moulin <phv.moulin@gmail.com>
2024-12-15 18:00:57 +0100

    build: :building_construction: move app.json to app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,4 +1,4 @@
-import { ConfigContext, ExpoConfig } from "expo/config"
+import { ExpoConfig } from "expo/config"
 
 const IS_PREVIEW = process.env.APP_VARIANT === "preview"
 const IS_DEV = !IS_PREVIEW
@@ -13,9 +13,59 @@ const getAppName = () => {
   return "Fallout Companion App"
 }
 
-export default ({ config }: ConfigContext): ExpoConfig => ({
-  ...config,
+export default (): ExpoConfig => ({
   name: getAppName(),
-  android: { package: getUniqueIdentifier() },
-  slug: "fca2"
+  slug: "fca2",
+  scheme: "fca2",
+  version: "2.2.0",
+  orientation: "landscape",
+  icon: "./assets/icon.png",
+  userInterfaceStyle: "light",
+  backgroundColor: "#021000",
+  primaryColor: "#021000",
+  splash: {
+    image: "./assets/splash.png",
+    resizeMode: "contain",
+    backgroundColor: "#021000"
+  },
+  newArchEnabled: true,
+  assetBundlePatterns: ["**/*"],
+  ios: {
+    supportsTablet: true,
+    buildNumber: "2.2.0"
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: "./assets/adaptive-icon.png",
+      backgroundColor: "#021000"
+    },
+    package: getUniqueIdentifier(),
+    versionCode: 510020200
+  },
+  androidStatusBar: {
+    hidden: true
+  },
+  androidNavigationBar: {
+    backgroundColor: "#021000"
+  },
+  web: {
+    favicon: "./assets/favicon.png",
+    bundler: "metro",
+    backgroundColor: "#021000"
+  },
+  plugins: ["expo-router"],
+  extra: {
+    router: {
+      origin: false
+    },
+    eas: {
+      projectId: "56bb3475-f8d1-4161-a940-9b7e02dc5cf0"
+    }
+  },
+  runtimeVersion: {
+    policy: "appVersion"
+  },
+  updates: {
+    url: "https://u.expo.dev/56bb3475-f8d1-4161-a940-9b7e02dc5cf0"
+  }
 })
