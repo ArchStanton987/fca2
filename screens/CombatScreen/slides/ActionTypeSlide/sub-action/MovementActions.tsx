c377b98901e46df7064fdd6a51bda6bf8f571674
ArchStanton987 <phv.moulin@gmail.com>
2025-05-10 11:05:11 +0200

    feat(combat): :sparkles: set default ap assigment on movement action

--- screens/CombatScreen/slides/ActionTypeSlide/sub-action/MovementActions.tsx
+++ screens/CombatScreen/slides/ActionTypeSlide/sub-action/MovementActions.tsx
@@ -1,16 +1,31 @@
 import actions from "lib/combat/const/actions"
+import getUseCases from "lib/get-use-cases"
 
 import List from "components/List"
 import ListItemSelectable from "components/ListItemSelectable"
 import ScrollSection from "components/Section/ScrollSection"
 import Txt from "components/Txt"
+import { useCharacter } from "contexts/CharacterContext"
 import { useActionApi, useActionForm } from "providers/ActionProvider"
+import { useCombat } from "providers/CombatProvider"
 
 const title = "action / pa / dist"
 
 export default function MovementActions() {
+  const useCases = getUseCases()
+  const { status } = useCharacter()
+  const { combat } = useCombat()
   const { actionSubtype } = useActionForm()
-  const { setActionSubtype } = useActionApi()
+  const { setActionSubtype, setForm } = useActionApi()
+
+  const onPressElement = (id: keyof typeof actions.movement.subtypes) => {
+    if (!combat) throw new Error("No combat found")
+    setActionSubtype(id)
+    const apCost = actions.movement.subtypes[id].apCost ?? status.currAp
+    setForm({ apCost })
+    useCases.combat.updateAction({ combat, payload: { apCost } })
+  }
+
   return (
     <ScrollSection style={{ flex: 1 }} title={title}>
       <List
@@ -20,7 +35,7 @@ export default function MovementActions() {
           <ListItemSelectable
             isSelected={actionSubtype === item.id}
             style={{ flexDirection: "row", justifyContent: "space-between" }}
-            onPress={() => setActionSubtype(item.id)}
+            onPress={() => onPressElement(item.id)}
           >
             <Txt style={{ flex: 1 }}>{item.label}</Txt>
             <Txt>{item.apCost ?? "-"}</Txt>
