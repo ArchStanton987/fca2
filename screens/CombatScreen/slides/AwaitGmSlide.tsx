31c449aa13d3e236f10ddbf0ef5d63625a0794a1
ArchStanton987 <phv.moulin@gmail.com>
2025-05-10 10:43:23 +0200

    feat(ui): :lipstick: add loader when waiting for gm

--- screens/CombatScreen/slides/AwaitGmSlide.tsx
+++ screens/CombatScreen/slides/AwaitGmSlide.tsx
@@ -1,7 +1,10 @@
+import { ActivityIndicator } from "react-native"
+
 import Section from "components/Section"
 import DrawerSlide from "components/Slides/DrawerSlide"
 import Spacer from "components/Spacer"
 import Txt from "components/Txt"
+import colors from "styles/colors"
 
 export default function AwaitGmSlide() {
   return (
@@ -11,7 +14,9 @@ export default function AwaitGmSlide() {
         contentContainerStyle={{ flex: 1, alignItems: "center", justifyContent: "center" }}
       >
         <Txt>En attente du MJ</Txt>
-        <Spacer y={10} />
+        <Spacer y={30} />
+        <ActivityIndicator size="large" color={colors.secColor} />
+        <Spacer y={30} />
         <Txt>Le MJ doit déterminer la difficulté de votre action...</Txt>
       </Section>
     </DrawerSlide>
