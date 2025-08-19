b03aee6b5beb3b4a85f772858359902d11ee8f6d
ArchStanton987 <phv.moulin@gmail.com>
2025-08-19 19:02:58 +0200

    fix(ui): :lipstick: fix can't see next button in update objects slide

--- components/UpdateObjects/UpdateObjects.styles.ts
+++ components/UpdateObjects/UpdateObjects.styles.ts
@@ -34,7 +34,6 @@ const styles = StyleSheet.create({
     alignItems: "center"
   },
   centeredSection: {
-    flex: 1,
     flexDirection: "row",
     justifyContent: "space-evenly",
     alignItems: "center"
