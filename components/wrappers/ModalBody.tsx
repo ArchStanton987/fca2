e789c17701164273a7b1dd93627ab461752fbb34
Pierre MOULIN <pierre.moulin@ageo.fr>
2025-11-05 13:55:51 +0100

    fix(ui): :lipstick: fix modal bg

--- components/wrappers/ModalBody.tsx
+++ components/wrappers/ModalBody.tsx
@@ -1,6 +1,8 @@
 import React from "react"
 import { View } from "react-native"
 
+import colors from "styles/colors"
+
 export default function ModalBody({ children }: { children: React.ReactNode }) {
-  return <View style={{ flex: 1, padding: 10 }}>{children}</View>
+  return <View style={{ flex: 1, padding: 10, backgroundColor: colors.primColor }}>{children}</View>
 }
